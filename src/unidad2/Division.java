fe98b7f3a85326bd0e0c0e71bb618a03247e6f33
CoviGitHub <queipo_6@hotmail.com>
2019-10-30 08:51:08 +0100

    Ejercicios 1 de la unidad 2 resuelto

--- src/unidad2/Division.java
+++ src/unidad2/Division.java
@@ -1,9 +1,13 @@
 package unidad2;
 
+
 public class Division {
-	
 	public static void main(String[] args) {
 		
+	 float num1 = 1234;
+	 float num2 = 532;
+	 System.out.println("1234/ 532 = "+ num1/num2);	 
+	 System.out.printf("1234/ 532 = %015.2f", (num1/num2));
+
 	}
-	
 }
