b9e1df2d1fec83205c2ab7597140375184560e05
CoviGitHub <queipo_6@hotmail.com>
2019-10-31 18:03:06 +0100

    Ejercicio 3 de la unidad 3 resuelto (corregido).

--- src/unidad3/Bisiesto.java
+++ src/unidad3/Bisiesto.java
@@ -9,7 +9,7 @@ public class Bisiesto {
 	public static void main(String[] args) throws IOException {
 		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 		System.out.println("Introduce un año cualquiera");
-		int año = (int) Long.parseLong(in.readLine());
+		int año = Integer.parseInt(in.readLine());
 		
 		if(año%4==0 && (año%100!=0 || año%400==0)) {
 				System.out.print("Es bisiesto");
